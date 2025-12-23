9cb0fdaf2a08951618d8a4e35f7a6b5ed5ff07a8
Rubel224603 <cserubelhosen@gmail.com>
2025-12-23 19:27:20 +0600

    import data by  command

---
+++ app/Console/Commands/ImportMongoDbUsers.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\File;
+
+class ImportMongoDbUsers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:import-mongo-db-users';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Import Mongo db Json int Mysql';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+        $path = storage_path('app/inceptionbd.users.json');
+        if (!File::exists($path)) {
+            $this->error("File not found! Make sure inceptionbd.users.json is in storage/app/");
+            return;
+        }
+        // Decode JSON into an array
+        $users = json_decode(File::get($path), true);
+
+        if (!is_array($users)) {
+            $this->error("Invalid JSON format.");
+            return;
+        }
+        foreach ($users as $user) {
+            User::updateOrCreate([
+                'email' => $user['email']
+            ], [
+                'name' => $user['name'] ?? 'No Name',
+                'email' => $user['email'],
+                'phone' => $user['phone'] ?? null,
+                'password' => $user['password'] ?? Hash::make('123456'),
+                'photo' => $user['photo'] ?? null,
+                'roll' => $user['roll'] ?? "student",
+                'status' => $user['status'] ?? "active",
+                'isVerified' => $user['isVerified'] ?? false,
+                'education' => $user['education'] ?? null,
+                'experience' => $user['experience'] ?? null,
+                'bio' => $user['bio'] ?? null,
+                'jobTitle' => $user['jobTitle'] ?? null,
+                // 'skills' => $user['skills'] ?? null,
+                'skills' => isset($u['skills']) && is_array($u['skills'])
+                    ? json_encode($u['skills'])
+                    : ($u['skills'] ?? null),
+                'facebookUrl' => $user['facebookUrl'] ?? null,
+                'githubUrl' => $user['githubUrl'] ?? null,
+                'linkedInUrl' => $user['linkedInUrl'] ?? null,
+                'twitterUrl' => $user['twitterUrl'] ?? null,
+                'youtubeUrl' => $user['youtubeUrl'] ?? null,
+                'mongodb_id' => $user['mongodb_id'] ?? null,
+                'created_at'   => isset($u['createdAt']['$date']) ? Carbon::parse($u['createdAt']['$date']) : now(),
+                'updated_at'   => isset($u['updatedAt']['$date']) ? Carbon::parse($u['updatedAt']['$date']) : now(),
+
+            ]);
+        }
+    }
+}
---
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password')->nullable();
+            $table->string('photo')->nullable();
+            $table->enum('roll', ['admin', 'teacher', 'student'])->default('student');
+            $table->string('status')->default('active');
+            $table->tinyInteger('isVerified')->default(0);
+            $table->text('education')->nullable();
+            $table->text('experience')->nullable();
+            $table->text('bio')->nullable();
+            $table->string('jobTitle')->nullable();
+            $table->string('skills')->nullable();
+            $table->string('facebookUrl')->nullable();
+            $table->string('githubUrl')->nullable();
+            $table->string('linkedInUrl')->nullable();
+            $table->string('twitterUrl')->nullable();
+            $table->string('youtubeUrl')->nullable();
+            $table->string('mongodb_id')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('sessions');
+    }
+};
